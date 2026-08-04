2d4ddb67891dc70e7f10ae8db72d0633ad81ad16
Javier Reyes <javier.reyes.gonzalez@maersk.com>
2026-08-04 14:36:40 -0600

    Changed the grocery_tracker.js file, since it has an error in the line 8, changed the comma (,) for a point (.)

--- groceryTracker/grocery_tracker.js
+++ groceryTracker/grocery_tracker.js
@@ -5,8 +5,8 @@ let amountgrocery3;
 function TotalAmountGroceries(){
     amountgrocery1 = parseFloat(document.getElementById('grocery1').value);
     amountgrocery2 = parseFloat(document.getElementById('grocery2').value);
-    amountgrocery3 = parseFloat(document.getElementById('grocery3'),value);
+    amountgrocery3 = parseFloat(document.getElementById('grocery3').value);
 
 let total = amountgrocery1 + amountgrocery2+ amountgrocery3;
-document.getElementById('result').innerText = "Total Grocery Amount: $(total)";
+document.getElementById('result').innerText = "Total Grocery Amount: $" + total.toFixed(2);
 }
