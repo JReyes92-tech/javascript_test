bdc9fb22f4d2005f70ca4f5bf4968bdce8f06c2c
Javier Reyes <javier.reyes.gonzalez@maersk.com>
2026-08-17 19:03:04 -0400

    Added the prices of all the restaurant menu

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes -$12', 'Eggs Benedict -$22.99', 'Oatmeal -21.99', 'Frittata -$15'];
+const mainCourseMenu = ['Steak -$30', 'Pasta -$15.99', 'Burger $20', 'Salmon $35.50'];
+const dessertMenu = ['Cake -$22.99', 'Ice Cream -$8', 'Pudding -$10', 'Fruit Salad -$12'];
  const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
         document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
  let mainCourseItem = '';
