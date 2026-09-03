a2a2b30364bca9e413f39cd40112de5b4aa626f8
Javier Reyes <javier.reyes.gonzalez@maersk.com>
2026-09-03 14:31:53 -0400

    Updated the html and js files with the clear all tasks button, to clear all the To Do list eventhough the tasks are completed or not

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -2,6 +2,7 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+const clearAllTasksBtn = document.getElementById("clearAllTasksBtn");
 let tasks = [];
 function addTask() {
             const taskText = taskInput.value.trim();
@@ -29,7 +30,12 @@ function clearCompletedTasks() {
     tasks = tasks.filter(task => !task.completed);
     displayTasks();
 }
+function clearAllTasks(){
+    tasks.length = 0;
+    displayTasks();
+}
 addTaskBtn.addEventListener("click", addTask);
 displayTasks();
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+clearAllTasksBtn.addEventListener("click", clearAllTasks);
 
